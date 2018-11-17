020fab1b103aa76d57ea7fdee4c44d58dd597272
Jonatan Liljestrand <jonatan.liljestrand@gmail.com>
2018-11-17 13:28:03 +0100

    Reduced dependencies. Removed Q thanks to async await. Removed Request in favor of axios.

--- controllers/sampleController.js
+++ controllers/sampleController.js
@@ -1,11 +1,6 @@
 // const sampleModule = require('../lib/sampleModule');
 
 exports.postSample = (req, res, next) => {
-  /*  const returnObject = sampleModule.sampleFunction(req, {
-    parameters: req.params,
-    body: req.body,
-  }); */
-
   res.send('returnObject');
   return next();
 };
